cfa8d962004ef23dd377d7c0bc89ca24f27383b2
RgMellon <renan_gian@hotmail.com>
2020-10-12 13:39:27 -0300

    add testes Logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,7 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { rendertWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a white label by default', () => {})
+  it('should render a white label by default', () => {
+    rendertWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    rendertWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
 })
