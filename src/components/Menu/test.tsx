df150a300ade6dc5c644c8e468d754c2fb75d5da
RgMellon <renan_gian@hotmail.com>
2020-10-17 11:48:03 -0300

    render the open/close mobile menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { rendertWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -8,11 +8,26 @@ describe('<Menu />', () => {
     rendertWithTheme(<Menu />)
 
     expect(screen.getByLabelText(/Open Menu/i)).toBeInTheDocument()
-
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
-
     expect(screen.getByLabelText(/Open shopping cart/i)).toBeInTheDocument()
-
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    rendertWithTheme(<Menu />)
+
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //clicar no botao de abrir o menu e verificar se ele abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
