a4ca81d4fd79991c4ab6061ad84afd8a0f5665dc
RgMellon <renan_gian@hotmail.com>
2020-10-20 09:06:28 -0300

    add as of styled-component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,16 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'small' | 'large' | 'medium'
   fullWidth?: boolean
   icon?: React.ReactElement
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 const Button = ({
   children,
