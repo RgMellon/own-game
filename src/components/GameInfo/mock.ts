16c41ca91ab3ba702ed6034137349cbe9cf02ff5
RgMellon <renan_gian@hotmail.com>
2020-12-02 10:45:24 -0300

    add page gameInfo

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Bordelands 3',
+  description:
+    'Experience the epic space strateggy games that refined the RTS genre',
+  price: '210,00'
+}
--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,15 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { rendertWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
+const props = {
+  title: 'My Game Tilte',
+  description: 'My Game Descriptio',
+  price: '210,00'
+}
+
 describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameInfo />)
+  it('should render game informations', () => {
+    rendertWithTheme(<GameInfo {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /GameInfo/i })
+      screen.getByRole('heading', { name: /My Game Tilte/i })
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByText('My Game Descriptio')).toBeInTheDocument()
+    expect(screen.getByText(/\$210,00/)).toBeInTheDocument()
+  })
+
+  it('should render Buttons', () => {
+    rendertWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /wishList/i })
+    ).toBeInTheDocument()
   })
 })
