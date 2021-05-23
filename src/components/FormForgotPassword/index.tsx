4e3d79f44466043a6bc1e81de786bb7e8ef1eed4
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2021-05-23 13:44:28 -0300

    create the forgot page

--- src/components/FormForgotPassword/index.tsx
+++ src/components/FormForgotPassword/index.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/client'
-import Link from 'next/link'
+// import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+import { Email, ErrorOutline } from '@styled-icons/material-outlined'
 
-import { FormLink, FormWrapper, FormLoading, FormError } from 'components/Form'
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 
-import { FieldErrors, sigInValidate } from 'utils/validations'
+import { FieldErrors } from 'utils/validations'
 
-import * as S from './styles'
+// import * as S from './styles'
 
-const FormSignIn = () => {
+const FormForgotPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
 
-  const [values, setValues] = useState({ email: '', password: '' })
+  const [values, setValues] = useState({ email: '' })
 
   const [loading, setLoading] = useState(false)
   const routes = useRouter()
@@ -31,7 +31,7 @@ const FormSignIn = () => {
     event.preventDefault()
     setLoading(true)
 
-    const errors = sigInValidate(values)
+    const errors = {} //validate after
 
     if (Object.keys(errors).length) {
       setFieldError(errors)
@@ -74,31 +74,12 @@ const FormSignIn = () => {
           error={fieldError?.email}
         />
 
-        <TextField
-          error={fieldError?.password}
-          name="password"
-          placeholder="Password"
-          type="password"
-          onInputChange={(v) => handleInput('password', v)}
-          icon={<Lock />}
-        />
-        <Link href="/forgot-password" passHref>
-          <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
-        </Link>
-
         <Button type="submit" size="large" fullWidth disabled={loading}>
-          {loading ? <FormLoading /> : <span>Sign in now</span>}
+          {loading ? <FormLoading /> : <span>Send email</span>}
         </Button>
-
-        <FormLink>
-          Don’t have an account?{' '}
-          <Link href="/sign-up">
-            <a>Sign up</a>
-          </Link>
-        </FormLink>
       </form>
     </FormWrapper>
   )
 }
 
-export default FormSignIn
+export default FormForgotPassword
---
+++ src/components/FormForgotPassword/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components'
+
+export const ForgotPassword = styled.a`
+  ${({ theme }) => css`
+    display: block;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-decoration: none;
+    text-align: right;
+  `}
+`
